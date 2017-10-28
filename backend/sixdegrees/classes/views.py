5d730f1e37474c9184cdb2b6bdf2a53a9a674098
hansen487 <hansen487@gmail.com>
2017-10-28 04:47:21 -0400

    added home page

--- backend/sixdegrees/classes/views.py
+++ backend/sixdegrees/classes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "classes/index.html")
+    return render(request, "classes/UI/Webpage/examples/landing-page.html")
