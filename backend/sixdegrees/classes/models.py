3c33d6e8b36214bb948ca21be788552cca971555
hansen487 <hansen487@gmail.com>
2017-10-28 05:31:57 -0400

    linked signup page

--- backend/sixdegrees/classes/models.py
+++ backend/sixdegrees/classes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    username = models.CharField(max_length = 128)
+    password = models.CharField(max_length = 128)
+    first_name = models.CharField(max_length = 128)
+    last_name = models.CharField(max_length = 128)
+    email = models.CharField(max_length = 128)
