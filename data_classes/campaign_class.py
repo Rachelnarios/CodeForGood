94fbdf51e7702c0f0e95610fd77f92b60068e85a
hansen487 <hansen487@gmail.com>
2017-10-27 23:31:04 -0400

    data structures skeleton

--- data_classes/campaign_class.py
+++ data_classes/campaign_class.py
@@ -8,7 +8,15 @@ class Campaign:
         self.NPO = NPO_input
         self.description = NPO.get_description()
         self.story = story
+        self.users = {}
     
     def get_story(self):
         return self.story
+        
+    def add_user(self, username, parent = None, size = '0'):
+        if self.users == {}:
+            self.users[username] = size             #size is a string
+        else:
+            parent_size = self.users[parent.get_username()]
+            self.users[username] = parent_size + size
     
--- data_classes/user_class.py
+++ data_classes/user_class.py
@@ -1,10 +1,25 @@
 from campaign_class import Campaign
 
 class User:
-    def __init__(self, name):
+    def __init__(self, name, username, password, parent):
         self.first, self.last = name.split()
-        self.parent = None
+        self.parent = parent
         self.children = 6 * []
-        self.login = None
-        self.password = None
+        self.username = username
+        self.password = password
         self.size = 0
+        self.campaign = parent.get_campaign()
+        
+    def get_username(self):
+        return self.username
+        
+    def get_campaign(self):
+        return self.campaign
+        
+    def add_user(self, user_name, user_username, user_password):
+        new_user = User(user_name, user_username, user_password)
+        self.children[self.size] = new_user
+        self.campaign.add_user(self.username, self.parent, str(self.size))
+        self.size += 1
+        
+                 
