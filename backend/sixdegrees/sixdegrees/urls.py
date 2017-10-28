d93b9c98826b2c4fa70863b71f83e90b7ea34c04
hansen487 <hansen487@gmail.com>
2017-10-28 06:50:21 -0400

    linked more sites

--- backend/sixdegrees/sixdegrees/urls.py
+++ backend/sixdegrees/sixdegrees/urls.py
@@ -7,13 +7,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     url(r'^accounts/login/', LoginView.as_view(template_name='classes/UI/Webpage/examples/userlogin.html'), name="userlogin"),
+    url(r'^accounts/nplogin/', LoginView.as_view(template_name='classes/UI/Webpage/examples/NPsignup-page.html'), name='np-signup'),
     url(r'^accounts/logout/', LogoutView.as_view(), name="user_logout"),
 #    url(r'^accounts/register/', CreateView.as_view(
 #            template_name='registration/register.html',
 #            form_class=UserCreationForm,
 #            success_url='/'
 #    )),
-    url(r'^accounts/register/', CreateView.as_view(), name = 'signup-page'),
+    url(r'^accounts/register/', CreateView.as_view(template_name='classes/UI/Webpage/examples/signup-page.html'), name = 'signup-page'),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('classes.urls')),
 ]
