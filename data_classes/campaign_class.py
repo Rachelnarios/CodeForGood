3095a52f20652a645f786b2ffc503fa9fc547a1e
hansen487 <hansen487@gmail.com>
2017-10-27 21:45:17 -0400

    changes

--- data_classes/campaign_class.py
+++ data_classes/campaign_class.py
@@ -1,4 +1,9 @@
 from user_class import User
+from NPO_class import NPO
 
 class Campaign:
-    
+    def __init__(self, name, catalyst, NPO_input):
+        self.campaign_name = name
+        self.catalyst = catalyst
+        self.NPO = NPO_input
+        self.description = NPO.get_description()
