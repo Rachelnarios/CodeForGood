c3bae0fbb586fe674319eb7052a88f602d6310b7
hansen487 <hansen487@gmail.com>
2017-10-28 03:19:22 -0400

    backend changes

--- backend/myproject/todos/models.py
+++ backend/myproject/todos/models.py
@@ -3,36 +3,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-class UserProfile(models.Model):  
-    user = models.OneToOneField(User)  
-    campaign = models.ForeignKey(Campaigns, null=True, blank=True)
+class NPOProfile(models.Model):   
+    username = models.CharField(max_length = 128)
+    password = models.CharField(max_length = 128)
+    name = models.CharField(max_length = 128)
+    description = models.TextField()
 
     def __str__(self):  
           return "%s's profile" % self.user  
-
-def create_user_profile(sender, instance, created, **kwargs):  
-    if created:  
-       profile, created = UserProfile.objects.get_or_create(user=instance)  
-
-post_save.connect(create_user_profile, sender=User) 
-
-
-class NPOProfile(models.Model):  
-    npo_user = models.OneToOneField(User)  
-   	campaign = models.ForeignKey(Campaigns, null=True, blank=True)  
-
-    def __str__(self):  
-          return "%s's profile" % self.user  
-
-def create_user_profile(sender, instance, created, **kwargs):  
-    if created:  
-       profile, created = UserProfile.objects.get_or_create(user=instance)  
-
-post_save.connect(create_user_profile, sender=User) 
-
 
 class Campaigns(models.Model):
-
     user = models.ForeignKey(User, null=True, blank=True)
     npo = models.ForeignKey(NPOProfile, null=True, blank=True)
     actions = models.CharField(max_length=128, null=False, blank=False)
@@ -41,3 +21,34 @@ class Campaigns(models.Model):
     def __str__(self):
         """this sets the default return for this object"""
         return self.description
+
+
+class UserProfile(models.Model):  
+    first_name = models.CharField(max_length = 128)
+    last_name = models.CharField(max_length = 128)
+    username = models.CharField(max_length = 128)
+    password = models.CharField(max_length = 128)
+    email = models.CharField(max_length = 128)
+
+    def __str__(self):  
+        return "%s's profile" % self.user  
+        
+    
+    
+          
+
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:  
+       profile, created = UserProfile.objects.get_or_create(user=instance)  
+
+post_save.connect(create_user_profile, sender=User) 
+
+
+
+def create_user_profile(sender, instance, created, **kwargs):  
+    if created:  
+       profile, created = UserProfile.objects.get_or_create(user=instance)  
+
+post_save.connect(create_user_profile, sender=User) 
+
+
